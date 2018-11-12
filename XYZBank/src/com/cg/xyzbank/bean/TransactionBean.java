a1483a3274a57850e5e30a77ca4d9522d994a951
mahalakshmi22021997 <mahapasumarthi@gmail.com>
2018-11-12 18:09:52 +0530

    Add files via upload

---
+++ XYZBank/src/com/cg/xyzbank/bean/TransactionBean.java
@@ -0,0 +1,58 @@
+package com.cg.xyzbank.bean;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="transaction1")
+public class TransactionBean {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private int id;
+	private Double deposit;
+	private Double withDraw;
+	private Double fundTransfer;
+	private String transactionType;
+	private Double amount;
+	
+	public Double getDeposit() {
+		return deposit;
+	}
+	public void setDeposit(Double deposit) {
+		this.deposit = deposit;
+	}
+	public Double getWithDraw() {
+		return withDraw;
+	}
+	public void setWithDraw(Double withDraw) {
+		this.withDraw = withDraw;
+	}
+	public Double getFundTransfer() {
+		return fundTransfer;
+	}
+	public void setFundTransfer(Double fundTransfer) {
+		this.fundTransfer = fundTransfer;
+	}
+	public String getTransactionType() {
+		return transactionType;
+	}
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+	public Double getAmount() {
+		return amount;
+	}
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+	@Override
+	public String toString() {
+		return "TransactionBean [deposit=" + deposit + ", withDraw=" + withDraw
+				+ ", fundTransfer=" + fundTransfer + ", transactionType="
+				+ transactionType + ", amount=" + amount + "]";
+	}
+	
+}
